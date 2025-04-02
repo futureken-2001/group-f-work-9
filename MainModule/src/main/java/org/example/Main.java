5cbf1533e79232a60878b8760bd4508ed95b60fa
Ntale3 <swamaduntale98@gmail.com>
2025-04-02 20:26:24 +0300

    added a module for running the logic

--- MainModule/src/main/java/org/example/Main.java
+++ MainModule/src/main/java/org/example/Main.java
@@ -1,20 +1,16 @@
-package org.ui;
+package org.example;
 
-import org.database.DatabaseConnect;
+import org.ui.AuthUI;
+import org.ui.Component;
 
 import javax.swing.*;
-import java.rmi.server.Skeleton;
-import java.sql.Connection;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        new Component();
 
-
-
-
-
-
+        SwingUtilities.invokeLater(()->{new AuthUI();});
     }
 }
